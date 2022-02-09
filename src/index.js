b9b3ed3a069a398f23cb8e49efed0e386edb265a
Rubens Batista <batistarubinho@gmail.com>
2022-02-08 23:48:53 -0300

    feat: add challenge's resolution

--- src/index.js
+++ src/index.js
@@ -10,19 +10,55 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const user = users.find(user => user.username === request.headers.username);
+  
+  if(!user) return response.status(404).json({ error: "User doesn't exists." });
+
+  request.user = user;
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(user.pro) return next();
+
+  const hasLessThanTenTodos = user.todos.length < 10;
+
+  if(hasLessThanTenTodos) return next(); 
+  
+  return response.status(403).json({ error: 'Limit of 10 todos exceeded.' });  
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  const user = users.find(user => user.username === username);
+  
+  if(!user) return response.status(404).json({ error: "User doesn't exists." });
+
+  const isValidUUID = validate(id);
+
+  if(!isValidUUID) return response.status(400).json({ error: "id is not a uuid's type."});
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) return response.status(404).json({ error: "Todo doesn't exists." });
+
+  request.todo = todo;
+  request.user = user;
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user) return response.status(404).json({ error: "User doesn't exists." });
+
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
